cf894cf5b4293241c3a8fe9056ddecb3ae63856d
Roman <mr.gnome91@gmail.com>
2015-10-23 19:03:15 +0300

    Project Initialized

---
+++ Assets/Scripts/ListObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ListObject : MonoBehaviour
+{
+	public string description;
+	public Sprite picture;
+	public GameObject viewObject;
+
+	public Image imageHolder;
+	public Text textHolder;
+
+	private Object3DController object3DController;
+
+	void Start()
+	{
+		imageHolder.sprite = picture;
+		textHolder.text = description;
+		object3DController = GameObject.Find("Object3DPlace").GetComponent<Object3DController>();
+	}
+
+	public void InstantiateObject()
+	{
+		object3DController.RemoveObject();
+		Instantiate( viewObject ).transform.SetParent( object3DController.transform );
+		object3DController.SetObjectName( description );
+	}
+}
---
+++ Assets/Scripts/Object3DController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Object3DController : MonoBehaviour
+{
+	public Text objectName;
+
+	public void ChangeAnimationState()
+	{
+		if( transform.childCount != 0 )
+		{
+			bool state = transform.GetChild(0).GetComponent<Animator>().enabled;
+			transform.GetChild(0).GetComponent<Animator>().enabled = !state;
+		}
+	}
+
+	public void RemoveObject()
+	{
+		if( transform.childCount != 0 )
+		{
+			Destroy( transform.GetChild(0).gameObject );
+			objectName.text = string.Empty;
+		}
+	}
+
+	public void SetObjectName(string name)
+	{
+		objectName.text = name;
+	}
+}
